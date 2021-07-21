22c70e2b43c9ffc974d3414a602b9f393df92b5e
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-07-21 12:34:21 -0300

    finalizada tela de edição de playlist

--- src/pages/components/CfitAdmin/CfitAdminPlaylists.jsx
+++ src/pages/components/CfitAdmin/CfitAdminPlaylists.jsx
@@ -1,36 +1,66 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/button-has-type */
 import ModalAdicionarPlaylist from "./CfitAdminPlaylists/ModalAdicionarPlaylist";
+import axios from 'axios';
+import { serverAddress } from '../../../util/Settings';
 
 const CfitAdminPlaylists = () => {
   const [modalVisible, setmodalVisible] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(true);
+  const [playlists, setplaylists] = React.useState([]);
+  const [playlistIdEditar, setplaylistIdEditar] = React.useState(-1);
+  const updatePlaylistIdEditar = (playlistId) => {
+    setplaylistIdEditar(parseInt(playlistId));
+  }
+  const getPlaylists = () => {
+    axios.get(`${serverAddress}playlist`).then((response) => {
+      setplaylists(response.data.map((res) => (
+        <tr className="border border-danger" key={res.pk}>
+          <td className="border border-danger">{res.titulo}</td>
+          <td className="border border-danger">{res.descricao}</td>
+          <td className="border border-danger"><button className="btn btn-primary" value={res.pk}
+            onClick={(e) => {
+              updatePlaylistIdEditar(e.target.value);
+              setmodalVisible(true);
+            }}
+          >EDITAR</button></td>
+        </tr>
+      )));
+      setLoading(false);
+    });
+  };
+  
+  React.useEffect(() => {
+    getPlaylists();
+  }, []);
+  if (isLoading) {
+    return <div><h1>Loading...</h1></div>;
+  }
+
   return (
     <div>
-      <div className="row">
+      <div className="row d-flex justify-content-between px-3">
         <h1 className="d-block col-4 m-2">PLAYLISTS</h1>
         {' '}
         <button className="m-2 col-4 btn btn-primary"
         onClick={() => {
           setmodalVisible(true);
         }}
-        > ADICIONAR NOVA</button>
+        >ADICIONAR NOVA</button>
       </div>
       <div className="p-2">
         <div style={{ height: '70vh', overflow: 'auto' }} className="rounded border border-danger">
-          <table className="table table-hover table-light table-borderd border border-danger rounded">
-            <thead className="border border-danger table-primary">
+          <table className="table table-hover table-dark table-borderd border border-danger rounded">
+            <thead className="border border-danger bg-secondary">
               <tr>
                 <th scope="col" className="border border-primary">TITULO</th>
                 <th scope="col" className="border border-primary">DESCRICAO</th>
-                <th className="col-1">EDITAR</th>
+                <th className="col-1 border border-primary"
+                >EDITAR</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="border border-danger">
-                <td className="border border-danger">Mark</td>
-                <td className="border border-danger">Otto</td>
-                <td className="border border-danger"><button className="btn btn-primary">EDITAR</button></td>
-              </tr>
+              {playlists}
             </tbody>
           </table>
           <div
@@ -39,7 +69,7 @@ const CfitAdminPlaylists = () => {
                 !modalVisible ? 'animate__fadeOutRight' : 'animate__fadeInLeft'
               }`}
           >
-            <ModalAdicionarPlaylist setmodalVisible={setmodalVisible} />
+            <ModalAdicionarPlaylist setmodalVisible={setmodalVisible} playlistIdEditar={playlistIdEditar} setplaylistIdEditar={setplaylistIdEditar}/>
           </div>
         </div>
       </div>
--- src/pages/components/CfitAdmin/CfitAdminPlaylists/ModalAdicionarPlaylist.jsx
+++ src/pages/components/CfitAdmin/CfitAdminPlaylists/ModalAdicionarPlaylist.jsx
@@ -1,8 +1,40 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-undef */
+import axios from 'axios';
+import { serverAddress } from '../../../../util/Settings';
+
 const ModalAdicionarPlaylist = (props) => {
   const [titulo, settitulo] = React.useState('');
   const [descricao, setdescricao] = React.useState('');
   const [imagem, setimagem] = React.useState('');
+  const [playlistId, setPlaylistId] = React.useState(props.playlistIdEditar);
+  const setCampos=(valores)=>{
+    settitulo(valores.titulo);
+    setdescricao(valores.descricao);
+    setimagem(valores.imagem);
+    setPlaylistId(valores.pk);
+  }
+  const getPlaylists = () => {
+    try{
+      console.log(props)
+      axios.get(`${serverAddress}playlist/${props.playlistIdEditar}`).then((response) => {
+        setCampos(response.data);
+      }).catch(()=>{
+        settitulo('');
+        setdescricao('');
+        setimagem('');
+        setPlaylistId(-1);
+      });
+    }catch(e){
+      setcampos({titulo: '', descricao: '', imagem: ''});
+      }
+    }
+  
+  
+
+  React.useEffect(() => {
+    getPlaylists();
+  }, [props.playlistIdEditar]); 
   return (
     <div>
       <div
@@ -20,12 +52,21 @@ const ModalAdicionarPlaylist = (props) => {
               type="button"
               onClick={() => {
                 props.setmodalVisible(false);
+                props.setplaylistIdEditar(-1);
               }}
             >
               FECHAR
             </button>
           </div>
           <div className="modal-body">
+            <input
+              type="text"
+              className="form-control my-3 wsi-shadow-light"
+              placeholder="TITULO"
+              value={playlistId}
+              onChange={(e) => settitulo(e.target.value)}
+              hidden
+            />
             <input
               type="text"
               className="form-control my-3 wsi-shadow-light"
@@ -47,11 +88,10 @@ const ModalAdicionarPlaylist = (props) => {
               value={imagem}
               onChange={(e) => setimagem(e.target.value)}
             />
-
             <button
               className="btn btn-primary wsi-shadow-primary"
             >
-              CADASTRAR
+              {playlistId == -1 ? 'CADASTRAR': 'SALVAR'}
             </button>
           </div>
         </div>
