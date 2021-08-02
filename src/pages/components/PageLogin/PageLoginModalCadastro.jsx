74f45fd63d183fac1c8449c50c20775774e2e2e0
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-08-02 10:09:00 -0300

    adicionada validação do formulario de cadastro

--- src/pages/components/PageLogin/PageLoginModalCadastro.jsx
+++ src/pages/components/PageLogin/PageLoginModalCadastro.jsx
@@ -1,81 +1,87 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-shadow */
 /* eslint-disable no-undef */
 /* eslint-disable react/button-has-type */
 /* eslint-disable linebreak-style */
-import axios from "axios";
-import { serverAddress } from "@/util/Settings";
+import axios from 'axios';
+import { serverAddress } from '@/util/Settings';
 
 const PageLoginModalCadastro = (props) => {
-  const [usernameCadastro, setusernameCadastro] = React.useState("");
-  const [passwordCadastro, setpasswordCadastro] = React.useState("");
-  const [error, seterror] = React.useState("");
+  const [usernameCadastro, setusernameCadastro] = React.useState('');
+  const [passwordCadastro, setpasswordCadastro] = React.useState('');
+  const [error, seterror] = React.useState('');
   const [showErrorCadastro, setshowErrorCadastro] = React.useState(false);
-  const [alertType, setalertType] = React.useState("danger");
-  const [confirmpasswordCadastro, setConfirmpasswordCadastro] = React.useState("");
+  const [alertType, setalertType] = React.useState('danger');
+  const [confirmpasswordCadastro, setConfirmpasswordCadastro] = React.useState('');
+  const [formValido, setformValido] = React.useState(false);
 
   const clearForm = (alertType, error) => {
     const oldAlertType = alertType;
-    const oldError =  error;
-    setusernameCadastro("");
-    setpasswordCadastro("");
-    setConfirmpasswordCadastro("");
+    const oldError = error;
+    setusernameCadastro('');
+    setpasswordCadastro('');
+    setConfirmpasswordCadastro('');
     showErrorCadastroMessage(oldError, oldAlertType);
-  }
-
+  };
 
   const clearData = () => {
     setshowErrorCadastro(false);
-    setalertType("");
-    setusernameCadastro("");
-    setpasswordCadastro("");
-    setConfirmpasswordCadastro("");
-    seterror("");
-  }
+    setalertType('');
+    setusernameCadastro('');
+    setpasswordCadastro('');
+    setConfirmpasswordCadastro('');
+    seterror('');
+  };
 
   const showErrorCadastroMessage = (message, type) => {
     setshowErrorCadastro(true);
     seterror(message);
     setalertType(type);
-  }
+  };
 
   const RegisterUser = () => {
-    axios.defaults.headers.common = { Authorization: ""};
-    axios
-      .post(`${serverAddress}user/`, { username: usernameCadastro, password: passwordCadastro })
-      .then((response) => {
-        if (response.status === 200 || response.data.success || response.status ==201) {
-          showErrorCadastroMessage("Cadastro efetuado com sucesso!", "success");
-          clearForm("success py-4 ", "Cadastro efetuado com sucesso!");
-        }else{
-          showErrorCadastroMessage(response.data.message, "danger");
-        }
-      })
-      .catch((error) => {       
-          showErrorCadastroMessage(error.response.data.message, "danger");
-      });
+    if (formValido) {
+      axios.defaults.headers.common = { Authorization: '' };
+      axios
+        .post(`${serverAddress}user/`, { username: usernameCadastro, password: passwordCadastro })
+        .then((response) => {
+          if (response.status === 200 || response.data.success || response.status == 201) {
+            showErrorCadastroMessage('Cadastro efetuado com sucesso!', 'success');
+            clearForm('success py-4 ', 'Cadastro efetuado com sucesso!');
+          } else {
+            showErrorCadastroMessage(response.data.message, 'danger');
+          }
+        })
+        .catch((error) => {
+          showErrorCadastroMessage(error.response.data.message, 'danger');
+        });
+    } else {
+      showErrorCadastroMessage('Preencha todos os campos!', 'danger');
+    }
   };
 
   React.useEffect(() => {
     if (confirmpasswordCadastro !== passwordCadastro) {
-      showErrorCadastroMessage("As senhas não conferem", "danger");
-    }else{
-      showErrorCadastroMessage("", "")
-      setshowErrorCadastro(false)
+      showErrorCadastroMessage('As senhas não conferem', 'danger');
+    } else {
+      showErrorCadastroMessage('', '');
+      setshowErrorCadastro(false);
+      setformValido(true);
     }
-
   }, [confirmpasswordCadastro, passwordCadastro]);
 
   React.useEffect(() => {
-    const listener = event => {
-      if (event.code === "Enter" || event.code === "NumpadEnter") {
-          if(usernameCadastro != "" && passwordCadastro != ""){
-            RegisterUser();
-          }
+    const listener = (event) => {
+      if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+        if (usernameCadastro != '' && passwordCadastro != '') {
+          RegisterUser();
+        }
       }
     };
-    document.addEventListener("keydown", listener);
+    document.addEventListener('keydown', listener);
     return () => {
-      document.removeEventListener("keydown", listener);
+      document.removeEventListener('keydown', listener);
     };
   }, [usernameCadastro, passwordCadastro]);
 
@@ -84,7 +90,7 @@ const PageLoginModalCadastro = (props) => {
       <div
         className="modal-dialog modal-lg  "
         role="document"
-        style={{ width: "100%" }}
+        style={{ width: '100%' }}
       >
         <div className="modal-content wsi-container-dark wsi-border-primary wsi-shadow-primary blur">
           <div className="modal-header">
@@ -143,14 +149,17 @@ const PageLoginModalCadastro = (props) => {
               alertType
             } alert-dismissible alert-dismissible-dark wsi-shadow-light my-4 rounded
             ${
-              showErrorCadastro ? " d-block py-4 " : "d-none"
-            }}`}>
+              showErrorCadastro ? ' d-block py-4 ' : 'd-none'
+            }}`}
+            >
               <h4>{error}</h4>
             </div>
             <div className="mt-5">
               <p>
-                Ja possui cadastro?{" "}
-                <button className="btn wsi-btn-secondary wsi-shadow-light btn-sm"
+                Ja possui cadastro?
+                {' '}
+                <button
+                  className="btn wsi-btn-secondary wsi-shadow-light btn-sm"
                   onClick={() => {
                     clearData();
                     props.setmodalVisible(false);
