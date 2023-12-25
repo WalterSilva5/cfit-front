d009b704fd0230f7c2d019cb6696fbf2fd1558fb
Walter Pereira Da Silva <waltersilva5@outlook.com>
2023-12-24 23:36:39 -0300

    fix nav component

--- src/components/nav.component.tsx
+++ src/components/nav.component.tsx
@@ -1,56 +1,47 @@
-import React from 'react';
 import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  Menu,
-  MenuItem,
-  Link,
-  Box,
-  Container,
-  Grid
-} from '@mui/material';
+	AppBar,
+	Toolbar,
+	IconButton,
+	Typography,
+	Button,
+	Link,
+	Box} from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link as RouterLink,
-  withRouter
+	Link as RouterLink,
 } from 'react-router-dom';
 
 export function NavComponent(props: any) {
-  const { history } = props;
-  return (
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{
-            mr: 2,
-            display: 'none'
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit" component="div">
-          <Link component={RouterLink} to="/" underline="none" color="inherit">
-            NOME_DO_APP
-          </Link>
-        </Typography>
-        <Box />
-        <Button sx={{ ml: '20px' }} color="inherit" onClick={() => history.push('/')}>
-          Home
-        </Button>
-        <Button color="inherit" onClick={() => history.push('/admin')}>
-          Admin
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
+	const { history } = props;
+	return (
+		<AppBar position="static">
+			<Toolbar variant="dense">
+				<IconButton
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					sx={{
+						mr: 2,
+						display: 'none'
+					}}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h6" color="inherit" component="div">
+					<Link component={RouterLink} to="/" underline="none" color="inherit">
+						NOME_DO_APP
+					</Link>
+				</Typography>
+				<Box />
+				<Button sx={{ ml: '20px' }} color="inherit" onClick={() => history.push('/')}>
+					Home
+				</Button>
+				<Button color="inherit" onClick={() => history.push('/admin')}>
+					Admin
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
 }
 
-export default withRouter(NavComponent);
+export default NavComponent;
